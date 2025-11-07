c65da537157613349377278a1dc723ad630e399a
kltzqu <kltz4074@gmail.com>
2025-11-07 14:37:59 +0200

    black skybox

--- src/engine.cpp
+++ src/engine.cpp
@@ -118,7 +118,7 @@ int main()
         ProcessInput(window); // input
         Update(); 
 
-        glClearColor(0.027f, 0.0f, 0.341f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         int width, height;
