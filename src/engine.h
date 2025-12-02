e91774695894470804ca21469dab8da17f15e84b
kltzqu <kltz4074@gmail.com>
2025-12-02 20:29:17 +0200

    removed doesn't need & work function from engine.h

--- src/engine.h
+++ src/engine.h
@@ -50,15 +50,6 @@ void SetDeltaTime(double dt);
 // Source - https://stackoverflow.com/q
 // Posted by Swatcat, modified by community. See post 'Timeline' for change history
 // Retrieved 2025-12-02, License - CC BY-SA 3.0
-inline void window(GLFWwindow* oldWindow, bool FullScreen, GLFWmonitor* monitor, int width, int height, char* title) {
-    glfwDestroyWindow(oldWindow);
-    if (!FullScreen) {
-        GLFWwindow* window = glfwCreateWindow(width, height, title, 0, 0);
-    } else {
-        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-        GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, title, monitor, 0);
-    }
-}
 } // namespace PointEngine
 
 #endif // ENGINE_H
